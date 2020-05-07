711a10196c734ef93334a948048c9e0f03b29fe5
dalijolijo <info@bitcore.cc>
2020-05-07 16:50:27 +0000

    fix

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -3,9 +3,9 @@
 apt install unzip
 mkdir ./bootstrap
 cd ./bootstrap
-wget https://github.com/LIMXTEC/BitCore/releases/download/0.90.9.1/z_bootstrap.zip
-unzip z_bootstrap.zip
-docker-compose -f docker-compose-electrumx.yml down
-cp -r blocks/ chainstate/ /home/bitcore
-docker-compose -f docker-compose-electrumx.yml up -d
-
+#wget https://github.com/LIMXTEC/BitCore/releases/download/0.90.9.1/z_bootstrap.zip
+#unzip z_bootstrap.zip
+cd ..
+./stop-electrumx.sh
+cp -r ./bootstrap/blocks/ ./bootstrap/chainstate/ /home/bitcore
+./start-electrumx.sh
