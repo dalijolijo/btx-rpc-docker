dfcaf6a909cf8983e4cd47312d9a3bd4bb97fd14
dalijolijo <info@bitcore.cc>
2020-09-29 09:38:51 +0000

    Update

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-apt install unzip
-mkdir ./bootstrap
+VERSION=0.90.9.6
+
+#Install needed packages
+dpkg-query -l unzip > /dev/null
+if [ $? -ne 0 ] ; then
+        apt-get install -y unzip
+fi
+
+if [ ! -d ./bootstrap ]; then
+   mkdir ./bootstrap
+fi
 cd ./bootstrap
-#wget https://github.com/LIMXTEC/BitCore/releases/download/0.90.9.1/z_bootstrap.zip
-#unzip z_bootstrap.zip
+
+if [ ! -f ./bootstrap.zip ]; then
+   wget https://github.com/LIMXTEC/BitCore/releases/download/${VERSION}/bootstrap.zip -O bootstrap.zip
+fi
+if [ ! -d ./blocks ]; then
+   unzip bootstrap.zip
+fi
+
 cd ..
 ./stop-electrumx.sh
 cp -r ./bootstrap/blocks/ ./bootstrap/chainstate/ /home/bitcore
